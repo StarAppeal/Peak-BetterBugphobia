c50c46d0e05956f51d7d40afe2734e282c955097
StarAppeal <info@starappeal.app>
2025-11-20 01:37:13 +0100

    Add BeeSwarm patch and update version to 1.0.4

---
+++ src/BetterBugphobia/BeeSwarmPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BetterBugphobia;
+
+[HarmonyPatch(typeof(BeeSwarm), "Start")]
+public class BeeSwarmPatch
+{
+    [HarmonyPrefix]
+    private static bool Prefix(BeeSwarm __instance)
+    {
+        Plugin.Log.LogInfo("BeeSwarm Prefix started!");
+        if (Plugin.OriginalBeeSwarmMaterial == null)
+        {
+            Plugin.Log.LogInfo("Saving original particles");
+            Plugin.OriginalBeeSwarmMaterial = __instance.beeParticles.GetComponent<ParticleSystemRenderer>().material;
+        }
+        
+        Plugin.ApplyBeeSwarmSettings(__instance);
+        Plugin.Log.LogInfo("BeeSwarm Prefix finished, preventing calling original method");
+        return false;
+    }
+}
--- src/BetterBugphobia/Plugin.cs
+++ src/BetterBugphobia/Plugin.cs
@@ -11,49 +11,60 @@ namespace BetterBugphobia;
 [BepInAutoPlugin]
 public partial class Plugin : BaseUnityPlugin
 {
+    private static List<Setting> mobList =
+    [
+        new() { MobType = typeof(Antlion), SettingName = "Antlion", OnSettingChanged = [OnSettingChanged] },
+        new()
+        {
+            MobType = typeof(BeeSwarm), SettingName = "Bees",
+            OnSettingChanged = [OnSettingChanged, OnBeesSettingChanged]
+        },
+        new() { MobType = typeof(Beetle), SettingName = "Beetle", OnSettingChanged = [OnSettingChanged] },
+        new() { MobType = typeof(Scorpion), SettingName = "Scorpion", OnSettingChanged = [OnSettingChanged] },
+        new() { MobType = typeof(Spider), SettingName = "Spider", OnSettingChanged = [OnSettingChanged] },
+        new() { MobType = typeof(Bugfix), SettingName = "Tick", OnSettingChanged = [OnSettingChanged] }
+    ];
+
     internal static ManualLogSource Log { get; private set; } = null!;
 
-    private List<Setting> mobList = [
-        new() {MobType = typeof(Spider), SettingName = "Spider"},
-        new () {MobType = typeof(Scorpion), SettingName = "Scorpion"},
-         new() {MobType = typeof(Antlion), SettingName = "Antlion"},
-        new () {MobType = typeof(Beetle), SettingName = "Beetle"}, 
-        new() {MobType = typeof(Bugfix), SettingName = "Tick"},
-        new () {MobType = typeof(BeeSwarm), SettingName = "Bees"},
-    ];
-    
     public static Dictionary<string, ConfigEntry<bool>> bugPhobiaMap = new();
-    
+
+    public static Material? OriginalBeeSwarmMaterial;
+
     private void Awake()
     {
         Log = Logger;
 
         foreach (var mob in mobList)
         {
-            var configEntry = Config.Bind("General", mob.SettingName, false, 
+            var configEntry = Config.Bind("General", mob.SettingName, false,
                 $"Toggle the {mob.SettingName}s appearance. false = normal appearance, true = Bing Bong");
-            configEntry.SettingChanged += OnSettingChanged;
+            foreach (var onSettingChanged in mob.OnSettingChanged)
+            {
+                configEntry.SettingChanged += onSettingChanged;
+            }
+
             bugPhobiaMap.Add(mob.MobType.Name, configEntry);
         }
 
         Harmony.CreateAndPatchAll(typeof(Plugin).Assembly);
-        
+
         Log.LogInfo($"Plugin {Name} is loaded!");
     }
 
-    private void OnSettingChanged(object? sender, EventArgs e)
+    private static void OnSettingChanged(object? sender, EventArgs e)
     {
         Log.LogInfo("Setting Changed!");
         ApplyAllBugphobiaSettings();
     }
-    
-    private void ApplyAllBugphobiaSettings()
+
+    private static void ApplyAllBugphobiaSettings()
     {
         foreach (var mobType in mobList)
         {
             foreach (var mob in FindObjectsOfType(mobType.MobType))
             {
-                var monsterComponent = mob as MonoBehaviour; 
+                var monsterComponent = mob as MonoBehaviour;
                 if (monsterComponent == null) continue;
                 if (monsterComponent.TryGetComponent<BugPhobia>(out var bugPhobiaComponent))
                 {
@@ -62,12 +73,36 @@ public partial class Plugin : BaseUnityPlugin
             }
         }
     }
-    
+
     public static void ApplyBugphobia(BugPhobia bugPhobiaComponent, string monsterName)
     {
         if (!bugPhobiaMap.TryGetValue(monsterName, out var configEntry)) return;
-        foreach(var go in bugPhobiaComponent.defaultGameObjects) go.SetActive(!configEntry.Value);
-        foreach(var go in bugPhobiaComponent.bugPhobiaGameObjects) go.SetActive(configEntry.Value);
+        foreach (var go in bugPhobiaComponent.defaultGameObjects) go.SetActive(!configEntry.Value);
+        foreach (var go in bugPhobiaComponent.bugPhobiaGameObjects) go.SetActive(configEntry.Value);
+    }
+
+    private static void OnBeesSettingChanged(object? sender, EventArgs e)
+    {
+        foreach (var beeSwarm in FindObjectsOfType<BeeSwarm>()) ApplyBeeSwarmSettings(beeSwarm);
+    }
+
+    public static void ApplyBeeSwarmSettings(BeeSwarm beeSwarm)
+    {
+        if (!bugPhobiaMap.TryGetValue("BeeSwarm", out var configEntry)) return;
+        {
+            if (configEntry.Value)
+            {
+                beeSwarm.beeParticles.GetComponent<ParticleSystemRenderer>().material = beeSwarm.bingBongMaterial;
+            }
+            else if (OriginalBeeSwarmMaterial != null)
+            {
+                beeSwarm.beeParticles.GetComponent<ParticleSystemRenderer>().material = OriginalBeeSwarmMaterial;
+            }
+            else
+            {
+                Log.LogWarning("Original BeeSwarm Material not found, ignoring setting for now");
+            }
+        }
     }
 }
 
@@ -75,4 +110,5 @@ public struct Setting
 {
     public Type MobType;
     public string SettingName;
+    public List<EventHandler> OnSettingChanged;
 }
