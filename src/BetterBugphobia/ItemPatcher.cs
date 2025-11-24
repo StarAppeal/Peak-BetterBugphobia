0569ddf94a2cbcd42b5da13329b1641b3875ac9f
StarAppeal <silkfan11@gmail.com>
2025-11-24 12:53:52 +0100

    Bump version to 1.1, refactor item handling and fix icon + overworld texture

---
+++ src/BetterBugphobia/ItemPatcher.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BetterBugphobia;
+
+[HarmonyPatch(typeof(Item.ItemUIData))]
+public class ItemPatcher
+{
+    private static readonly Dictionary<string, Type> ItemNameToMobTypeMap = new()
+    {
+        { "Tick", typeof(Bugfix) },
+        { "Scorpion", typeof(Scorpion) }
+    };
+
+    [HarmonyPatch("GetIcon")]
+    [HarmonyPostfix]
+    static void Postfix(Item.ItemUIData __instance, ref Texture2D __result)
+    {
+        if (ItemNameToMobTypeMap.TryGetValue(__instance.itemName, out var mobType))
+        {
+            if (Plugin.bugPhobiaMap.TryGetValue(mobType, out var configEntry))
+            {
+                __result = configEntry.Value ? __instance.altIcon : __instance.icon;
+            }
+        }
+    }
+}
